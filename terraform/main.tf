2f0d7e50938505388df957ca3e99c21894f1a8fe
Eduard Poghosyan <omian.tech@gmail.com>
2025-10-10 20:21:24 +0400

    updated terraform to create an EC2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -227,62 +227,6 @@ resource "aws_security_group" "ec2_sg" {
   )
 }
 
-# IAM role for EC2 instance
-resource "aws_iam_role" "ec2_role" {
-  name = "${var.project_name}-${var.environment}-ec2-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  tags = local.common_tags
-}
-
-# IAM policy for ECR access
-resource "aws_iam_role_policy" "ecr_policy" {
-  name = "${var.project_name}-${var.environment}-ecr-policy"
-  role = aws_iam_role.ec2_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Attach SSM policy for Session Manager access
-resource "aws_iam_role_policy_attachment" "ssm_policy" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-# IAM instance profile
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "${var.project_name}-${var.environment}-ec2-profile"
-  role = aws_iam_role.ec2_role.name
-
-  tags = local.common_tags
-}
-
 # EC2 Instance
 resource "aws_instance" "app_server" {
   ami                    = var.ami_id
